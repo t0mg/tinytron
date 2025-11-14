c4c670c3d5af6f1f1d0ae7107434ee0c697927c4
t0mg <tom@t0m.fr>
2025-11-14 08:55:14 +0100

    fix

--- src/VideoPlayer/AVIParser.cpp
+++ src/VideoPlayer/AVIParser.cpp
@@ -51,6 +51,21 @@ typedef struct
   unsigned int dwLength;
 } MainAVIHeader;
 
+// http://www.fastgraph.com/help/avi_header_format.html
+typedef struct
+{
+  char fccType[4];
+  char fccHandler[4];
+  unsigned int dwFlags;
+  unsigned short wPriority;
+  unsigned short wLanguage;
+  unsigned int dwInitialFrames;
+  unsigned int dwScale;
+  unsigned int dwRate;
+  unsigned int dwStart;
+  unsigned int dwLength;
+  unsigned int dwSuggestedBufferSize;
+} AVIStreamHeader;
 
 bool AVIParser::open()
 {
@@ -99,18 +114,108 @@ bool AVIParser::open()
 
       if (strncmp(listType, "hdrl", 4) == 0)
       {
-        // This is the header list, which contains the 'avih' chunk with the frame rate
-        ChunkHeader avihHeader;
-        readChunk(mFile, &avihHeader);
-        if (strncmp(avihHeader.chunkId, "avih", 4) == 0)
+        // We are inside the 'hdrl' LIST chunk. Its content starts at ftell(mFile)
+        // and ends at listContentPosition + header.chunkSize.
+        long hdrlContentRemaining = header.chunkSize - 4; // -4 for 'hdrl' type already read
+
+        while (hdrlContentRemaining > 0 && !feof(mFile) && !ferror(mFile))
         {
-          MainAVIHeader avih;
-          fread(&avih, sizeof(MainAVIHeader), 1, mFile);
-          mFrameRate = (float)avih.dwRate / avih.dwScale;
-          Serial.printf("Frame rate: %f\n", mFrameRate);
+          ChunkHeader subHeader;
+          long bytesReadForSubHeader = fread(&subHeader, 1, sizeof(ChunkHeader), mFile);
+          if (bytesReadForSubHeader != sizeof(ChunkHeader)) {
+              // Error or EOF
+              break;
+          }
+          hdrlContentRemaining -= bytesReadForSubHeader;
+
+          long subChunkDataSize = subHeader.chunkSize;
+          long subChunkTotalSize = subChunkDataSize;
+          if (subChunkTotalSize % 2 != 0) { // Account for padding
+              subChunkTotalSize++;
+          }
+
+          // Process the sub-chunk
+          if (strncmp(subHeader.chunkId, "avih", 4) == 0)
+          {
+            // We don't need to read avih content.
+            fseek(mFile, subChunkDataSize, SEEK_CUR);
+            hdrlContentRemaining -= subChunkTotalSize;
+          }
+          else if (strncmp(subHeader.chunkId, "LIST", 4) == 0)
+          {
+            char subListType[4];
+            long bytesReadForSubListType = fread(&subListType, 1, 4, mFile);
+            if (bytesReadForSubListType != 4) {
+                // Error or EOF
+                break;
+            }
+            subChunkDataSize -= bytesReadForSubListType; // Account for 'strl' type read
+
+            if (strncmp(subListType, "strl", 4) == 0)
+            {
+              long strlContentRemaining = subChunkDataSize;
+              while (strlContentRemaining > 0 && !feof(mFile) && !ferror(mFile))
+              {
+                ChunkHeader strhHeader;
+                long bytesReadForStrhHeader = fread(&strhHeader, 1, sizeof(ChunkHeader), mFile);
+                if (bytesReadForStrhHeader != sizeof(ChunkHeader)) {
+                    // Error or EOF
+                    break;
+                }
+                strlContentRemaining -= bytesReadForStrhHeader;
+
+                long strhDataSize = strhHeader.chunkSize;
+                long strhTotalSize = strhDataSize;
+                if (strhTotalSize % 2 != 0) { // Account for padding
+                    strhTotalSize++;
+                }
+
+                if (strncmp(strhHeader.chunkId, "strh", 4) == 0)
+                {
+                  AVIStreamHeader strh;
+                  long bytesReadForStrh = fread(&strh, 1, sizeof(AVIStreamHeader), mFile);
+                  if (bytesReadForStrh != sizeof(AVIStreamHeader)) {
+                      // Error or EOF
+                      break;
+                  }
+                  strhDataSize -= bytesReadForStrh; // Account for strh struct read
+
+                  if (strncmp(strh.fccType, "vids", 4) == 0)
+                  {
+                    if (strh.dwScale == 0) {
+                      Serial.println("Warning: dwScale is 0, can't calculate framerate.");
+                      mFrameRate = 0;
+                    } else {
+                      mFrameRate = (float)strh.dwRate / strh.dwScale;
+                      Serial.printf("Frame rate: %f\n", mFrameRate);
+                    }
+                  }
+                  fseek(mFile, strhDataSize, SEEK_CUR); // Skip remaining strh data
+                  strlContentRemaining -= strhTotalSize;
+                }
+                else
+                {
+                  // Not 'strh', skip its content
+                  fseek(mFile, strhDataSize, SEEK_CUR);
+                  strlContentRemaining -= strhTotalSize;
+                }
+              }
+              hdrlContentRemaining -= subChunkTotalSize; // All of 'strl' content handled
+            }
+            else
+            {
+              // Not 'strl', skip the rest of this LIST chunk's content
+              fseek(mFile, subChunkDataSize, SEEK_CUR);
+              hdrlContentRemaining -= subChunkTotalSize;
+            }
+          }
+          else
+          {
+            // Not 'avih' or 'LIST', skip its content
+            fseek(mFile, subChunkDataSize, SEEK_CUR);
+            hdrlContentRemaining -= subChunkTotalSize;
+          }
         }
-        // We've processed what we need from the 'hdrl' list, so skip to the end of it.
-        fseek(mFile, listContentPosition + header.chunkSize, SEEK_SET);
       }
       else if (strncmp(listType, "movi", 4) == 0)
       {
